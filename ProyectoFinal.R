096c9e3c3396e094c7630d74f4519ba5cac6cdea
OscarCanongo <oscarcanongo@outlook.com>
2021-01-18 10:54:30 -0600

    Datasets

---
+++ ProyectoFinal.R
@@ -0,0 +1,4 @@
+#Poner ruta propia
+covid <- read.csv("~/Desktop/Santander-DataScience/Data/Covid.csv")
+h1n1 <- read.csv("~/Desktop/Santander-DataScience/Data/H1N1.csv")
+ebola <- read.csv("~/Desktop/Santander-DataScience/Data/ebola.csv")
